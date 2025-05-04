5454db4592908b90e5417195e4ed1ab772345764
Nithyasri26 <83579336+Nithyasri26@users.noreply.github.com>
2025-05-04 17:43:44 +0530

    Add files via upload

---
+++ 5-spring-wiring/src/main/java/com/cg/SavingsAccRepoImpl.java
@@ -0,0 +1,15 @@
+package com.cg;
+
+public class SavingsAccRepoImpl implements AccountRepository {
+
+	public SavingsAccRepoImpl() {
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("constructor savings");
+	}
+	
+	public String credit(float amt) {
+		return "Hi your salary of "+amt+" credited in ur savings Account";
+	}
+
+}
