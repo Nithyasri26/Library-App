d0617312f3383cef7d1981852f30546aa77c8263
Nithyasri26 <83579336+Nithyasri26@users.noreply.github.com>
2025-05-04 17:44:17 +0530

    Add files via upload

---
+++ 6-spring-wiring-annotation/src/main/java/com/cg/Client.java
@@ -0,0 +1,15 @@
+package com.cg;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+	
+	public static void main(String[] args) {
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("beans.xml");
+		
+		AccountService service=(AccountService) ctx.getBean("accservice");
+		System.out.println(service.creditService(30000));
+	}
+
+}
