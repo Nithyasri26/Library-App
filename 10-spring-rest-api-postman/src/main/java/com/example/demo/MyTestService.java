8f0b9c8fc464786e7d2e362fb21eb74a942cb300
Nithyasri26 <83579336+Nithyasri26@users.noreply.github.com>
2025-05-04 17:46:50 +0530

    Add files via upload

---
+++ 10-spring-rest-api-postman/src/main/java/com/example/demo/MyTestService.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyTestService {
+	
+	public MyTestService() {
+		System.out.println("constructor");
+	}
+
+}
